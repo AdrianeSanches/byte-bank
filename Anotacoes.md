78a9b2b10466d5b053a217d37548efb44ef5d926
AdrianeSS <adriane_silvasanches@hotmail.com>
2025-09-29 23:56:57 -0300

    feat: configurando o ts-config

--- Anotacoes.md
+++ Anotacoes.md
@@ -1,7 +1,12 @@
 Navegar para pasta JS e rodar o comando tsc bytebank.ts para o TS fazer a conversão do arquivo TS para JS.
 Ele vai fazer a compilação do arquivo e vai gerar dentro da pasta JS o arquivo bytebank.js, porque ele fez a conversão do código em em TS para JS.
 Sempre que for feita uma alteração no TS, é necessário recompilar o arquivo para que ele reflita no JS.
+Esse cenário são para projetos que não possuem o arquivo de configuração do TS - tsconfig.json
 
 Dist: distribuição. Nome mais comum dado em projetos web à pastas que de fato representam o conjunto de arquivos que vão para o ambiete final (de produção).
 
 SRC: source/fonte/código-fonte. É o nome comum dado à pasta de desenvolvimento de um projeto.
+
+Ts-config: atquivo de configuração do TS e ele diz como o compilador do TS deve se comportar no projeto. Você pode criar o arquivo direto na mão, mas também pode rodar o comando tsc --init na raíz do projeto.
+
+tsc -w: o w é de watch, ele fica monitorando a pasta SRC (q foi definida no ts-config) e toda vez que um arquivo TS for modificado, ele vai compilar e gerar o arquivo JS correspondente automaticamente
--- dist/js/bytebank.js
+++ dist/js/bytebank.js
@@ -1,12 +1,11 @@
-// export {}; // incluído para resolver o problema Cannot redeclare block-scoped variable 'saldoTS'. Isso acontece porque ao gerar o arquivo em JS, ele duplica o nome da variável
-var saldoTS = 3000;
+let saldoTS = 3000;
 // para que a variável elementoSaldo nao apresente erro por conta de ser possível retornar um valor vazio, há duas formas de defensiva: a primeira é incluir as HTMLElement ao final do elemento, porém é recomendado fazer somente se você tiver certeza que o elemento existe. Caso seja possível ele não existir, o recomendado é validar se o elementoSaldo é diferente de null utilizando um if (como no comentário logo abaixo)
-var elementoSaldo = document.querySelector(".saldo-valor .valor");
+const elementoSaldo = document.querySelector(".saldo-valor .valor");
 elementoSaldo.textContent = saldoTS.toString();
 // if (elementoSaldo != null) {
 //     elementoSaldo.textContent = saldoTS.toString();
 // }
-var elementoFormularioTS = document.querySelector(".block-nova-transacao form");
+const elementoFormularioTS = document.querySelector(".block-nova-transacao form");
 elementoFormularioTS.addEventListener("submit", function (event) {
     event.preventDefault(); // toda vez que um formulário é submetido a página é carreda. Esse função evita o recarregamento da página quando o formulário é submetido, e dá acesso às informações do formulário
     // verifica se todos os itens do formulario foram preenchidos, tornando-o válido. O item required no html do formulário é o que faz essa validação
@@ -14,12 +13,12 @@ elementoFormularioTS.addEventListener("submit", function (event) {
         alert("Por favor, preencha todos os campos da transação.");
         return;
     }
-    var inputTipoTransacao = elementoFormularioTS.querySelector("#tipoTransacao");
-    var inputValor = elementoFormularioTS.querySelector("#valor");
-    var inputData = elementoFormularioTS.querySelector("#data");
-    var tipoTransacao = inputTipoTransacao.value;
-    var valor = inputValor.valueAsNumber; // valueAsNumber é uma propriedade existente nos Inputs
-    var data = new Date(inputData.value);
+    const inputTipoTransacao = elementoFormularioTS.querySelector("#tipoTransacao");
+    const inputValor = elementoFormularioTS.querySelector("#valor");
+    const inputData = elementoFormularioTS.querySelector("#data");
+    let tipoTransacao = inputTipoTransacao.value;
+    let valor = inputValor.valueAsNumber; // valueAsNumber é uma propriedade existente nos Inputs
+    let data = new Date(inputData.value);
     if (tipoTransacao === "Depósito") {
         saldoTS += valor;
     }
@@ -32,7 +31,7 @@ elementoFormularioTS.addEventListener("submit", function (event) {
         return;
     }
     elementoSaldo.textContent = saldoTS.toString();
-    var novaTransacao = {
+    const novaTransacao = {
         tipoTransacao: tipoTransacao,
         valor: valor,
         data: data,
